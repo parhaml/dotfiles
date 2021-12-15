02c9b6b09c9da962e3158ae9d042325a413b24de
Lee Parham <lee.parham@shopify.com>
2021-12-15 15:27:00 -0600

    Update symlink

--- install.sh
+++ install.sh
@@ -9,5 +9,5 @@ fi
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
-ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -sf ~/dotfiles/zshrc ~/.zshrc
 
