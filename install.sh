458ed4feb3ab846df2133e98cd4e9dc5ec94e3da
Lee Parham <lee.parham@shopify.com>
2021-12-15 16:36:56 -0600

    Oops

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if test ! $(which omz); then
   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
 fi
 
-sudo apt purge neovim vim
+echo "Y" | sudo apt purge neovim vim
 sudo apt install vim
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
