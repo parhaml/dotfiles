6b70ab2552c342bdf96eff2606100fab136117f6
Lee Parham <lee.parham@shopify.com>
2021-12-15 16:12:10 -0600

    Use vim

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ if test ! $(which omz); then
   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
 fi
 
+sudo apt purge neovim vim
+sudo apt install vim
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
 rm -rf $HOME/.vimrc
