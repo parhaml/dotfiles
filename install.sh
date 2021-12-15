2a4de730e68d5ac98a107fe0dfcc2ea900871b92
Lee Parham <lee.parham@shopify.com>
2021-12-15 15:34:06 -0600

    Updated

--- install.sh
+++ install.sh
@@ -7,8 +7,12 @@ if test ! $(which omz); then
   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
 fi
 
+if ! command -v silversearcher-ag &> /dev/null; then
+  sudo apt-get install silversearcher-ag
+fi
+
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
 ln -sf ~/dotfiles/zshrc ~/.zshrc
-ln -sf ~/dotfiles/zshrc ~/.vimrc
+ln -sf ~/dotfiles/vimrc ~/.vimrc
 
