5117c2570d841a9d8aeaf056a7d7c072fe4c5ddc
Lee Parham <lee.parham@shopify.com>
2021-12-20 12:41:39 -0600

    Homebrew and Ag

--- install.sh
+++ install.sh
@@ -5,6 +5,17 @@ if test ! $(which omz); then
   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
 fi
 
+# Homebrew
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+test -r ~/.bash_profile && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >>~/.bash_profile
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/spin/.zprofile
+
+
+# Make Ag in vim
+brew install the_silver_searcher
+
+
 echo "Y" | sudo apt purge neovim
 sudo apt install vim
 
